85e138c6a4e1557f7849c69af8d168cf73c6e99e
Oli Larkin <olilarkin@googlemail.com>
2018-02-08 11:49:17 +0000

    DEFAULT_FPS = 60

--- IPlug/IGraphics/IGraphicsConstants.h
+++ IPlug/IGraphics/IGraphicsConstants.h
@@ -2,7 +2,7 @@
 
 #include "IPlugPlatform.h"
 
-static const int DEFAULT_FPS = 25;
+static const int DEFAULT_FPS = 60;
 
 // If not dirty for this many timer ticks, we call OnGUIIDle.
 // Only looked at if USE_IDLE_CALLS is defined.
